8ea351b59b34d18226402ef892a23b70c2464f4f
surerever <49303845+surerever@users.noreply.github.com>
2024-11-11 23:25:46 +0300

    Update test_pokemon.py

--- Python + Pytest + Requests/tests/test_pokemon.py
+++ Python + Pytest + Requests/tests/test_pokemon.py
@@ -8,9 +8,7 @@ def test_trainer_name(key, value):
     response_parametrize = requests.get(url = f'{URL}/trainers?trainer_id=13088')
     assert response_parametrize.json()["data"][0][key] == value    
 
-@pytest.mark.parametrize("endpoint, status_code", [
-    ("/trainers", 200)
-])
+@pytest.mark.parametrize("endpoint, status_code", [("/trainers", 200)])
 def test_status_code(endpoint, status_code):
     response = requests.get(url=f"{URL}{endpoint}")
     assert response.status_code == status_code
